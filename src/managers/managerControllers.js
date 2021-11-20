70aee8af93233af202005a246c86fc43a0a52d48
tanhng <anhnt@tokyotechlab.com>
2021-11-20 17:12:28 +0700

    implement

--- src/managers/managerControllers.js
+++ src/managers/managerControllers.js
@@ -3,26 +3,55 @@
 let express = require("express");
 let router = new express.Router();
 
-let {register} = require("./managerServices");
-let {registerValidator} = require("./managerValidators");
+let {register, signIn, isLogging} = require("./managerServices");
+let {registerValidator, loginValidator} = require("./managerValidators");
 
-router.post("/managers", async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   try {
     let validator = await registerValidator(req);
     if (validator !== null) {
       return res.send({message: validator});
     } else {
       let registed = await register(req.body);
-      if (registed == true) {
-        return res.send({message: "Register successfully."});
+      if (registed) {
+        return res.send({
+          message: "Register successfully.",
+          registed
+        });
       } else {
         return res.send({message: "Email has been used."});
       }
     }    
   } catch (error) {
-    console.log("aeraer", error)
+    console.log(error)
     return res.status(500).send({error: "Server Error"});
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  try {
+    let isLogged = await isLogging(req);
+    if (isLogged === true) {
+      return res.send({message: "You are logged in."});
+    }
+    let validator = await loginValidator(req);
+    if (validator !== null) {
+      return res.send({message: validator});
+    }
+    let signIned = await signIn(req)
+    if (signIned === false) {
+      return res.send({message: "Email or Password is incorrect"});
+    } else {
+      return res.send({
+        message: "Sign In successfully.",
+        signIned
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({error: "Server Error"});
+  }
+});
+
 
 module.exports = router;
