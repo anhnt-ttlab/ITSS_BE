9036ce0f33d6d6a6f8014c99f8a0c21e2e8f6dec
tanhng <anhnt@tokyotechlab.com>
2021-11-26 22:17:32 +0700

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 var app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://itss-fe.herokuapp.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
