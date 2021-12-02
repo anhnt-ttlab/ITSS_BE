7051f5b1d104323015909f9ab69b489431e4ea08
irepvn_intern_k62_quang <quanglm@datascience.com.vn>
2021-12-02 15:21:07 +0700

    recreate db

--- db/migration/main.js
+++ db/migration/main.js
@@ -1,53 +1,59 @@
-var mysql = require('mysql');
-var dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' });
+var mysql = require("mysql");
+var dotenv = require("dotenv");
+dotenv.config({ path: "../../.env" });
 
 var con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
-  var sql = "CREATE TABLE managers (manager_id INT AUTO_INCREMENT, full_name VARCHAR(255), email VARCHAR(255), password VARCHAR(500), PRIMARY KEY (manager_id))";
+  var sql =
+    "CREATE TABLE managers (manager_id INT AUTO_INCREMENT, full_name VARCHAR(255), email VARCHAR(255), password VARCHAR(500), PRIMARY KEY (manager_id))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table managers created");
   });
 
-  var sql = "CREATE TABLE talents (talent_id INT AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255), avatar VARCHAR(2000), manager_id INT, PRIMARY KEY (talent_id), FOREIGN KEY (manager_id) REFERENCES managers(manager_id))";
+  var sql =
+    "CREATE TABLE talents (talent_id INT AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255), avatar VARCHAR(2000), manager_id INT, PRIMARY KEY (talent_id), FOREIGN KEY (manager_id) REFERENCES managers(manager_id) ON DELETE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table talents created");
   });
 
-  var sql = "CREATE TABLE courses (course_id INT AUTO_INCREMENT, course_name VARCHAR(255), time VARCHAR(255), creator_id INT, PRIMARY KEY (course_id), FOREIGN KEY (creator_id) REFERENCES managers(manager_id))";
+  var sql =
+    "CREATE TABLE courses (course_id INT AUTO_INCREMENT, course_name VARCHAR(255), time VARCHAR(255), creator_id INT, PRIMARY KEY (course_id), FOREIGN KEY (creator_id) REFERENCES managers(manager_id) ON DELETE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table courses created");
   });
 
-  var sql = "CREATE TABLE lessons (lesson_id INT AUTO_INCREMENT, lesson_name VARCHAR(255), time datetime, course_id INT, PRIMARY KEY (lesson_id), FOREIGN KEY (course_id) REFERENCES courses(course_id))";
+  var sql =
+    "CREATE TABLE lessons (lesson_id INT AUTO_INCREMENT, lesson_name VARCHAR(255), time datetime, course_id INT, PRIMARY KEY (lesson_id), FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table lessons created");
   });
 
-  var sql = "CREATE TABLE scores (score INT, talent_id INT, lesson_id INT, course_id INT, FOREIGN KEY (talent_id) REFERENCES talents(talent_id), FOREIGN KEY (lesson_id) REFERENCES lessons(lesson_id), FOREIGN KEY (course_id) REFERENCES courses(course_id))";
+  var sql =
+    "CREATE TABLE scores (score INT, talent_id INT, lesson_id INT, course_id INT, FOREIGN KEY (talent_id) REFERENCES talents(talent_id) ON DELETE CASCADE, FOREIGN KEY (lesson_id) REFERENCES lessons(lesson_id) ON DELETE CASCADE, FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table scores created");
   });
 
-  var sql = "CREATE TABLE schedules (mean_score FLOAT, talent_id INT, course_id INT, FOREIGN KEY (talent_id) REFERENCES talents(talent_id), FOREIGN KEY (course_id) REFERENCES courses(course_id))";
+  var sql =
+    "CREATE TABLE schedules (mean_score FLOAT, talent_id INT, course_id INT, FOREIGN KEY (talent_id) REFERENCES talents(talent_id) ON DELETE CASCADE, FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table schedules created");
   });
 
-  return
+  return;
 });
